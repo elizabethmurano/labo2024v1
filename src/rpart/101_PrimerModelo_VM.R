ab66684f7a5049fbe6bfe796300fb60beaf4afa5
elizabethmurano <eliza.murano@gmail.com>
2024-04-03 12:43:36 -0300

    otro commit

--- src/rpart/101_PrimerModelo_VM.R
+++ src/rpart/101_PrimerModelo_VM.R
@@ -7,7 +7,7 @@ require("rpart")
 require("rpart.plot")
 
 # Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("X:\\gdrive\\labo2024v1\\") # Establezco el Working Directory
+setwd("~/buckets/b1/") # Establezco el Working Directory
 
 # cargo el dataset
 dataset <- fread("./datasets/dataset_pequeno.csv")
