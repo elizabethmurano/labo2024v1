a2a03a78fae4cbaac283c99c2d8b18a53ba7421c
elizabethmurano <163559265+elizabethmurano@users.noreply.github.com>
2024-05-01 20:58:03 -0300

    501 y 541
    version 2 de boruta comentado delete clase 01

--- src/workflow-01/501_workflow_boruta_v1_15_16.r
+++ src/workflow-01/501_workflow_boruta_v1_15_16.r
@@ -136,7 +136,7 @@ FE_historia_guantesblancos <- function( pmyexp, pinputexps, pserver="local")
   if( -1 == (param_local <- exp_init( pmyexp, pinputexps, pserver ))$resultado ) return( 0 )# linea fija
   
   
-  param_local$meta$script <- "/src/workflow-01/541_FE_historia_boruta_v1.r"
+  param_local$meta$script <- "/src/workflow-01/541_FE_historia_boruta_v2.r"
   
   param_local$lag1 <- TRUE 
   param_local$lag2 <- FALSE # no me engraso con los lags de orden 2
